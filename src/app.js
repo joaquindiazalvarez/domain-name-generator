922ce26b8d8e72cf76f844484cd14f83f7cef866
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2022-02-14 01:28:47 +0000

    realized excercise first version

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#domainNames").innerHTML = getDomain();
 };
+function getDomain() {
+  let str = "";
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net", ".io", ".us", ".cl"];
+  let printDomain = () => {
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < extension.length; l++) {
+            str += pronoun[i] + adj[j] + noun[k] + extension[l] + "<br>";
+          }
+        }
+      }
+    }
+    return str;
+  };
+  return printDomain();
+}
+console.log(getDomain());
